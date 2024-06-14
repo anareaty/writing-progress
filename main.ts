cb3eef44997ffb35bcbbdaad9061c1831c96c09c
anareaty <reatymain@gmail.com>
2024-06-15 00:53:54 +0400

    create view

--- main.ts
+++ main.ts
@@ -1,34 +1,119 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, ItemView, WorkspaceLeaf, Workspace } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface WritingProgressPluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: WritingProgressPluginSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+
+
+
+
+
+
+/* VIEW */
+
+export const WRITING_PROGRESS_VIEW_TYPE = "example-view";
+
+export class WritingProgressView extends ItemView {
+  constructor(leaf: WorkspaceLeaf) {
+    super(leaf);
+  }
+
+  getViewType() {
+    return WRITING_PROGRESS_VIEW_TYPE;
+  }
+
+  getDisplayText() {
+    return "Example view";
+  }
+
+
+  wordCountPluginActive() {
+	return this.app.internalPlugins.plugins["word-count"].enabled
+  }
+
+  getActiveFile() {
+	return this.app.workspace.getActiveFile()
+  }
+
+
+  getWordCount() {
+	return this.app.internalPlugins.plugins["word-count"].instance.wordCount
+  }
+
+
+
+
+  async onOpen() {
+	const container = this.containerEl.children[1];
+    container.empty();
+
+
+	if (!this.getActiveFile()) {
+		container.createEl("h4", { text: "No file open" });
+
+
+	} else if (!this.wordCountPluginActive()) {
+		container.createEl("h4", { text: "Word count plugin disabled" });
+
+
+	} else {
+		container.createEl("h4", { text: "Example view" });
+
+	}
+    
+  }
+
+  async onClose() {
+    // Nothing to clean up.
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default class WritingProgressPlugin extends Plugin {
+	settings: WritingProgressPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
+		
+		
 
 		// This creates an icon in the left ribbon.
+		/*
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
 		});
+		*/
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		//ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+		/*
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
+		*/
 
 		// This adds a simple command that can be triggered anywhere
+		/*
 		this.addCommand({
 			id: 'open-sample-modal-simple',
 			name: 'Open sample modal (simple)',
@@ -36,7 +121,9 @@ export default class MyPlugin extends Plugin {
 				new SampleModal(this.app).open();
 			}
 		});
+		*/
 		// This adds an editor command that can perform some operation on the current editor instance
+		/*
 		this.addCommand({
 			id: 'sample-editor-command',
 			name: 'Sample editor command',
@@ -45,7 +132,9 @@ export default class MyPlugin extends Plugin {
 				editor.replaceSelection('Sample Editor Command');
 			}
 		});
+		*/
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
+		/*
 		this.addCommand({
 			id: 'open-sample-modal-complex',
 			name: 'Open sample modal (complex)',
@@ -64,23 +153,89 @@ export default class MyPlugin extends Plugin {
 				}
 			}
 		});
-
+		*/
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
+		/*
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
 			console.log('click', evt);
 		});
+		*/
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+		/*
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		*/
+
+
+
+
+		this.registerView(
+			WRITING_PROGRESS_VIEW_TYPE,
+			(leaf) => new WritingProgressView(leaf)
+		);
+
+		this.activateProgressView(false);
+
+		
+	  /*
+		this.addRibbonIcon("dice", "Activate view", () => {
+			this.activateProgressView();
+		});
+	*/	
+
+	this.addCommand({
+		id: 'open-writing-progress-view',
+		name: 'Открыть панель прогресса',
+		callback: () => {
+			this.activateProgressView(true);
+		}
+	});
+		
+
+		
 	}
 
 	onunload() {
 
 	}
+
+
+
+
+
+	async activateProgressView(reveal:boolean) {
+		this.app.workspace.onLayoutReady(async () => {
+
+			const { workspace } = this.app;
+
+			let leaf: WorkspaceLeaf | null = null;
+			const leaves = workspace.getLeavesOfType(WRITING_PROGRESS_VIEW_TYPE);
+
+			if (leaves.length > 0) {
+				// A leaf with our view already exists, use that
+				leaf = leaves[0];
+			} else {
+				// Our view could not be found in the workspace, create a new leaf
+				// in the right sidebar for it
+				leaf = workspace.getRightLeaf(false);
+				await leaf.setViewState({ type: WRITING_PROGRESS_VIEW_TYPE, active: reveal });
+			}
+
+			if (reveal) {
+				// "Reveal" the leaf in case it is in a collapsed sidebar
+				workspace.revealLeaf(leaf);
+			}
+		});
+	}
+
+
+
+
+	  
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -108,9 +263,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: WritingProgressPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: WritingProgressPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
